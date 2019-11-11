95e93490e30929b2f708b86f86d351438d25d2e9
dumbcoder7 <2017.aditya.raute@ves.ac.in>
2019-11-11 18:27:59 +0530

    MOBVIEW

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,24 @@ import React from 'react';
 import classes from './SideDrawer.module.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Logo from '../../Logo/Logo';
+import Auxiliary from '../../../hoc/Auxiliary';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const sideDrawer = () => {
+const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open){
+        attachedClasses[1]=classes.Open;
+    }
     return(
-        <div className={classes.SideDrawer}>
-            <Logo height="11%" />
-            <nav >
-                <NavigationItems />
-            </nav>
-        </div>
+        <Auxiliary>
+            <Backdrop show={props.open} close={props.closed}/>
+            <div className={attachedClasses.join(' ')} >
+                <Logo height="11%" />
+                <nav >
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Auxiliary>
     )
 }
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,15 +2,17 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
+
 const toolbar = (props) =>(
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
         <Logo height="100%"/>
         <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
     
-);
+); 
 
 export default toolbar;
